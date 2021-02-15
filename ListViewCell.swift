e47d9b8817d70df0023abb1862f3e6a83c143477
Zain Ahmed <44906047+thezainahmed98@users.noreply.github.com>
2021-02-15 14:11:43 -0600

    Add files via upload

---
+++ ListViewCell.swift
@@ -0,0 +1,11 @@
+//
+//  ListViewCell.swift
+//  Smart Camera
+//
+//  Created by Zain Ahmed on 8/24/20.
+//  Copyright © 2020 Zain Ahmed. All rights reserved.
+//
+
+import Foundation
+
+
---
+++ TableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  TableViewCell.swift
+//  Smart Camera
+//
+//  Created by Zain Ahmed on 8/24/20.
+//  Copyright © 2020 Zain Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var objectLabel: UILabel!
+    @IBOutlet weak var confidenceLabel: UILabel!
+
+}
